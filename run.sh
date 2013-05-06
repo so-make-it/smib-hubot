cd0814a24c2d0eafc9f85090b45b9a055d8f13cb
Benjie Gillam <benjie@jemjie.com>
2013-05-06 21:46:00 +0000

    Unflood, Benjie/Bracken, !alias.

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 export HUBOT_IRC_SERVER=irc.freenode.net
 export HUBOT_IRC_ROOMS="#southackton,#somakeit,#smibtest"
 export HUBOT_IRC_NICK="smib2"
-bin/hubot -a irc -n smib2
+export HUBOT_IRC_UNFLOOD="true"
+
+export HUBOT_AUTH_ADMIN="1367874408762,1367874475602"
+bin/hubot -a irc -n smib2 --alias '!'
