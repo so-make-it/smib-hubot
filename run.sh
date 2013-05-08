3b2cfb81254530ef92b08998c5d1ae9c82540bf2
Benjie Gillam <benjie@jemjie.com>
2013-05-08 21:14:45 +0000

    Make some configuration variables public.

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
+# Put defaults here
+export HUBOT_IRC_SERVER="irc.freenode.net"
+export HUBOT_IRC_ROOMS="#southackton,#somakeit,#smibtest"
+export HUBOT_IRC_NICK="yoshibot"
+export HUBOT_IRC_UNFLOOD="true"
+export HUBOT_ANNOUNCE_ROOMS="$HUBOT_IRC_ROOMS"
+export PORT=5555
+export HUBOT_URL="http://members.somakeit.org.uk:$PORT/"
+# Local 'env' file overrides these and adds more
 . ./env
-bin/hubot -a irc2 -n yoshibot --alias '!'
+bin/hubot -a irc2 -n $HUBOT_IRC_NICK --alias '!'
